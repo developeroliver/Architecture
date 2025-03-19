966388ae004d650f5362ada6179df98300d17f5d
developeroliver <developpeur.oliver@gmail.com>
2025-03-19 20:10:22 +0100

    Initial Commit

---
+++ UserMVVM/UserMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserMVVMApp.swift
+//  UserMVVM
+//
+//  Created by olivier geiger on 19/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct UserMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
